944e4d5fb3f36625df9a201b36103eea137eb527
Ese Okonofua <eseokonofua95@gmail.com>
2019-01-29 00:01:37 -0800

    App complete

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import PokemonList from '../pokemonList';
+import PokemonTypesMap from '../pokemonTypes';
 
 function PokemonPicker(props) {
     return (
-        <select className='pokemonPicker' id={props.id} onChange={props.handleChange} value={props.choice}>
+        <select className='pokemonPicker' id={props.id} 
+        
+        onChange={props.handleChange} value={props.choice}>
             {
                 props.pokemonList.map ( data => { 
                     return <option key={data.id} value={data.id}>{data.name}</option>;
@@ -15,25 +18,69 @@ function PokemonPicker(props) {
 }
 
 function PokemonCard(props) {
-
     if (props.pokemon) {
-        var myStyle = {
-            backgroundImage: `url(${props.pokemon.sprites.front_default}) `,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            width: '100%',
-            height: '45%'
-        };
+        var arrowUp = <span className='arrows'>&#8593;</span>;
+        var arrowDown = <span className='arrows'>&#8595;</span>;
+
+        var compArr = null;
+        if(props.vs) {
+            compArr = [];
+            props.pokemon.stats.map( (item, index) => compArr.push(item.base_stat - props.vs.stats[index].base_stat) )
+        }
 
         return (
             <div  className='pokemonCard'>
-                <h1>{props.pokemon.name}</h1>
-                <div style={myStyle} className='pokemonImage'></div>
-                <div className='pokemonTypes'>
-                    {props.pokemon.types.map ((item, index) => <label key={index}>{item.type.name}</label>)}
+                <h1 className='pokemonName'>{props.pokemon.name}</h1>
+                <div class='pokemonImageContainer'>
+                    <img className='pokemonImage' src={props.pokemon.sprites.front_default}></img>
                 </div>
-                <div className='pokemonStats'></div>
+                <div className='pokemonTypes'>
+                    {
+                        props.pokemon.types.map ((item, index) => { 
+                            var myTypeStyle = {
+                                display: 'inline-block',
+                                textAlign: 'center',
+                                minWidth: '55px',
+                                padding: '2px',
+                                marginRight: '5px',
+                                borderRadius: '5px',
+                                border: '1px solid white',
+                                color: 'white',
+                                backgroundColor: PokemonTypesMap[item.type.name]
+                            };
+
+                            return (<label style={myTypeStyle} key={index}>{item.type.name}</label>)
+                        })
+                    }
+                </div>
+                <div className='pokemonStats'>
+                    {props.pokemon.stats.map( (item, index ) => {
+                        var arrow = null;
+                        var arrowStyle = null;
+                        var color = null;
+                        if(compArr) {
+                            if(compArr[index] > 0) {
+                                color = 'green';
+                                arrow = arrowUp;
+                            }
+                            else if(compArr[index] < 0) {
+                                color = 'red';
+                                arrow = arrowDown;
+                            }
+                            arrowStyle = { color };
+                        }
+
+                        return (
+                            <div className='stat' key={index}>
+                                <h3 className='statName'>{item.stat.name}</h3>
+                                <div style={arrowStyle} className='statNumber'>
+                                    <span>{item.base_stat}</span>
+                                    {arrow}
+                                </div>                            
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         );
     }
@@ -76,7 +123,7 @@ export default class App extends Component {
                 </section>
                 <section className='cardSection'>
                     <PokemonPicker id='pokemonTwo' handleChange={this.handleChange} pokemonList={this.PokemonList} choice={this.state.PokemonTwo} />
-                    <PokemonCard pokemon={this.state.PokemonTwoData} />
+                    <PokemonCard pokemon={this.state.PokemonTwoData} vs={this.state.PokemonOneData} />
                 </section>
             </div>
         );
@@ -91,14 +138,12 @@ export default class App extends Component {
 
     loadPokemonOneData() {
         this.getPokemon(this.state.PokemonOne).then( res => {
-            console.log(this.getPokemonData(res.data));
             this.setState({ PokemonOneData: this.getPokemonData(res.data) });
         });
     }
 
     loadPokemonTwoData() {
         this.getPokemon(this.state.PokemonTwo).then( res => {
-            console.log(this.getPokemonData(res.data));
             this.setState({ PokemonTwoData: this.getPokemonData(res.data) });
         });
     }
---
+++ src/pokemonTypes.js
@@ -0,0 +1,19 @@
+export default {
+    "normal": "grey",
+    "fire": "red",
+    "water": "blue",
+    "electric": "goldenrod",
+    "grass": "green",
+    "ice": "dodgerblue",
+    "fighting": "crimson",
+    "poison": "purple",
+    "ground": "silver",
+    "flying": "skyblue",
+    "psychic": "pink",
+    "bug": "lightgreen",
+    "rock": "coral",
+    "ghost": "darkpurple",
+    "dragon": "mediumslateblue",
+    "fairy": "darkseagreen",
+    "steel": "lightsteelblue"
+}
